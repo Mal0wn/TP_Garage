47fe3227fd80e0a2b363dae33a257e76a40f7601
Marine M <malomclt@gmail.com>
2022-04-07 11:32:34 +0200

    create class Moto

---
+++ Classes/Moto.ts
@@ -0,0 +1,24 @@
+import { Vehicule } from "./Vehicule";
+
+export class Moto implements Vehicule {
+	marque: string;
+	couleur: string;
+	type: string;
+	
+	avancer(): void {
+		
+	}
+	reculer(): void {
+		
+	}
+	freiner(): void {
+		
+	}
+	afficherLeVehicule(): void {
+		
+	}
+
+
+
+
+}
